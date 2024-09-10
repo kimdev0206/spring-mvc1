9b37eedda14896a4ae8e1bfa9b202ae2871ef9d7
Yongki Kim <kimdev0206@gmail.com>
2024-09-10 19:38:43 +0900

    add: 애플리케이션

---
+++ src/main/java/study/spring_mvc1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package study.spring_mvc1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMvcApplication.class);
+	}
+
+}
---
+++ src/test/java/study/spring_mvc1/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_mvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
